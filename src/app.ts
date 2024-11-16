281e4ef32ba1b01a7bedb5b37c45bce24f0caea1
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2024-11-16 18:47:06 +0530

    test CICD

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // API routes
 app.get('/health', (req: Request, res: Response) => {
-  res.status(200).json({ status: 'Payment Service Online' });
+  res.status(200).json({ status: 'Payment service online' });
 });
 
 app.use('/v1', router);
