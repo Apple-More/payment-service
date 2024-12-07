7db2358a3ffb6501b103d386c4a94cfb7a66be79
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2024-12-08 01:56:27 +0530

    updated payment intent

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import { createPayment, getPaymentsByCustomer, getPaymentById, getAllPayments, g
 const router = Router();
 
 // customer routes
-router.post('/customer/payment-intent', createPaymentIntent);
 router.post('/customer/payments', createPayment);
 router.get('/customer/:customer_Id/payments', getPaymentsByCustomer);
 
@@ -15,5 +14,6 @@ router.get('/admin/statistics/payments', getPaymentStatistics);
 
 // stripe webhook
 router.post('/public/stripe-webhook', confirmPayment);
+router.post('/public/payment-intent', createPaymentIntent);
 
 export default router;
