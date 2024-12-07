72cab537928b776ebc62e62774defca1a734b255
Nipun-Milinda <nipunmilinda@gmail.com>
2024-12-07 11:28:01 +0530

    configure allowRoles middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express';
 import { createPayment, getPaymentsByCustomer, getPaymentById, getAllPayments, getPaymentStatistics, createPaymentIntent, confirmPayment } from '../controllers/payment-controller';
+import allowRoles from '../middlewares/allow-roles';
 
 const router = Router();
 
 // customer routes
-router.post('/customer/payment-intent', createPaymentIntent);
-router.post('/customer/payments', createPayment);
-router.get('/customer/:customer_Id/payments', getPaymentsByCustomer);
+router.post('/customer/payment-intent', allowRoles('Customer'), createPaymentIntent);
+router.post('/customer/payments', allowRoles('Customer'), createPayment);
+router.get('/customer/:customer_Id/payments', allowRoles('Customer'), getPaymentsByCustomer);
 
 // admin routes
-router.get('/admin/payments', getAllPayments);
-router.get('/admin/payments/:payment_Id', getPaymentById);
-router.get('/admin/statistics/payments', getPaymentStatistics);
+router.get('/admin/payments', allowRoles('Admin', 'SuperAdmin'), getAllPayments);
+router.get('/admin/payments/:payment_Id', allowRoles('Admin', 'SuperAdmin'), getPaymentById);
+router.get('/admin/statistics/payments', allowRoles('Admin', 'SuperAdmin'), getPaymentStatistics);
 
 // stripe webhook
 router.post('/public/stripe-webhook', confirmPayment);
